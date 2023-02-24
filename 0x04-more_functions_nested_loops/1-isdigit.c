c2c7a0c2c6c55e3d8e1c55602423f196eacd5a26
Slyzeal <nasirslyzeal@gmail.com>
2023-02-23 17:17:50 -0800

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/*
+* "_isdigit" - Check if 0-9 are digits
+* Description: _isdigit
+* @c:integer
+* Return: 1, and 0
+*/
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
