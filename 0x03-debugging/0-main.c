d18c7bd38c133c5d7d63e3cd4962b0b73abd17fc
Slyzeal <nasirslyzeal@gmail.com>
2023-02-24 16:39:00 -0800

    modified

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*main - debugging
-* Return : 0
-*
+* main - Test for positive or negative
+* Return: 0
 */
-int main(void);
+int main(void)
 {
 int i;
 i = 0;
 positive_or_negative(i);
 return (0);
 }
+
